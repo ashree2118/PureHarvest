782723d7654b62d522da42c006dc43623ba618e0
ashree2118 <ashree2118@gmail.com>
2025-03-21 23:05:48 +0530

    Update main.jsx

--- farmowner/src/main.jsx
+++ farmowner/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hero from './components/Hero.jsx'
+import Navbar from './components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Hero />
+    <Navbar />
   </StrictMode>,
 )
